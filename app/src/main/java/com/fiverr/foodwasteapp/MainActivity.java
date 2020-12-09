008fd5c9f82a617371cf1831e40a2388393a5a51
kcordovas <kcordovas77@gmail.com>
2020-12-09 15:28:54 -0500

    Create activity List Foundation with few design and added databinding in main activity too.

--- app/src/main/java/com/fiverr/foodwasteapp/MainActivity.java
+++ app/src/main/java/com/fiverr/foodwasteapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.fiverr.foodwasteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.fiverr.foodwasteapp.activity.list.ListFoundationActivity;
+import com.fiverr.foodwasteapp.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.buttonSignIn.setOnClickListener(view -> {
+            final Intent intent = new Intent(this, ListFoundationActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/fiverr/foodwasteapp/activity/list/ListFoundationActivity.java
@@ -0,0 +1,16 @@
+package com.fiverr.foodwasteapp.activity.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fiverr.foodwasteapp.R;
+
+public class ListFoundationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_foundation);
+    }
+}
